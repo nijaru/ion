1a7293b496df0411c864c984a28bf042898bbfde
Nick Russo <nijaru7@gmail.com>
2026-02-23 03:46:34 -0800

    ui: add StatusBar widget

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,6 +6,7 @@
 pub mod code_block;
 pub mod conversation;
 pub mod diff_view;
+pub mod status_bar;
 pub mod streaming;
 pub mod style;
 pub mod tool_call;
@@ -13,5 +14,6 @@ pub mod tool_call;
 pub use code_block::CodeBlock;
 pub use conversation::{ConversationEntry, ConversationView, EntryRole};
 pub use diff_view::DiffView;
+pub use status_bar::StatusBar;
 pub use streaming::StreamingText;
 pub use tool_call::{ToolCallView, ToolState};
