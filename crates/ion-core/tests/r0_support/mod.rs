8ee7cbbe393c64a9bb78755f927490faee7d0ffb
Nick <nijaru7@gmail.com>
2026-09-12 12:53:54 -0700

    test: export named R0 task result aliases

--- crates/ion-core/tests/r0_support/mod.rs
+++ crates/ion-core/tests/r0_support/mod.rs
@@ -3,7 +3,8 @@ mod task;
 
 pub use store::{PrototypeTaskStore, StoreError, TaskId, TaskStatus};
 pub use task::{
-    AbortPlan, BoxFuture, Completion, RunningTask, SharedStore, TaskContext, TaskError, TaskKind,
-    TaskRegistry, TerminalPlan, abort_task, create_task, execute_task,
-    execute_task_with_cancellation, mark_cancel, recover_task, shared_store, task,
+    AbortPlan, BoxFuture, Completion, RunningTask, SharedStore, TaskAbortResult, TaskContext,
+    TaskError, TaskKind, TaskRegistry, TaskRunResult, TaskStatus as _, TerminalPlan, abort_task,
+    create_task, execute_task, execute_task_with_cancellation, mark_cancel, recover_task, shared_store,
+    task,
 };
